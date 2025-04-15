8a9cdddfe5179cc7f586a1b9ef21c937a6f614c0
Subodh <s372144@studentrs.cdu.edu.au>
2025-04-15 17:27:02 +0930

    added pest/disease instances as a list

--- proj_mango/app_mango/data.py
+++ proj_mango/app_mango/data.py
@@ -2,26 +2,47 @@ from models import PestInfo, DiseaseInfo
 
 # -- Adding Pests -- #
 
-pest1 = PestInfo (
-    id=1,
-    name="Fruit Fly",
-    short_description="A major pest affecting mangoes.",
-    long_description="Fruit flies lay eggs inside the fruit, causing rotting and reducing yield.",
-    image_url="fruitfly.jpg"
-)
+pests = [
+    PestInfo(
+        id=1,
+        name="Fruit Fly",
+        short_description="A major pest affecting mangoes.",
+        long_description="Fruit flies lay eggs inside the fruit, causing rotting and reducing yield.",
+        image_url="fruitfly.jpg"
+    ),
 
+  PestInfo(
+        id=2,
+        name="name2",
+        short_description="A major pest affecting mangoes.",
+        long_description="Fruit flies lay eggs inside the fruit, causing rotting and reducing yield.",
+        image_url="fruitfly.jpg"
+    ),
 
-
+]
 
 
 
 
 
 # -- Adding Diseases -- #
-disease1 = DiseaseInfo(
-    id=1,
-    name="Disease1",
-    short_description="short_description",
-    long_description="Long_description",
-    image_url=""
-)
+
+diseases = [
+    
+DiseaseInfo(
+        id=1,
+        name="Powdery Mildew",
+        short_description="A fungal disease affecting mango trees.",
+        long_description="Powdery mildew causes white, powdery spots on leaves and fruit, reducing plant health.",
+        image_url="powdery_mildew.jpg"
+),
+
+DiseaseInfo(
+        id=2,
+        name="Disease2",
+        short_description="Disease 2 desc",
+        long_description="Powdery mildew causes white, powdery spots on leaves and fruit, reducing plant health.",
+        image_url="powdery_mildew.jpg"
+),
+
+]
