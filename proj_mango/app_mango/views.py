b401e614329da1e6132f553d0c663084a8a329fe
Subodh <s372144@studentrs.cdu.edu.au>
2025-04-15 21:21:41 +0930

    rewrote views.py based on new instances

--- proj_mango/app_mango/views.py
+++ proj_mango/app_mango/views.py
@@ -1,37 +1,39 @@
 from django.shortcuts import render
+from models import PestInfo, DiseaseInfo
 from .data import pest_list, disease_list
 from django.http import HttpResponseNotFound
 
 # Create your views here.
 
-# from django.shortcuts import render
-from .data import pest_list, disease_list
-
+# Home Page View
 def home(request):
-    return render(request, 'app_mango/home.html') # For home page
-
-def pest_list_view(request):
-    context = {
-        'pests': pest_list,
-        'diseases': disease_list
-    }
-    return render(request, 'app_mango/pest_list.html', context) # for pest/disease list
+    return render(request, 'app_mango/home.html')
 
 
-def pest_detail_view(request, item_id):
-    item = next((p for p in pest_list if p.id == item_id), None)
-    if item is None:
-        item = next((d for d in disease_list if d.id == item_id), None)
-    if item is None:
-        return HttpResponseNotFound("Page not Found") # Displays 404 page
-    return render(request, 'app_mango/pest_detail.html', {'item': item})
+# Pest & Disease Combined List View
+def pest_disease_list(request):
+    return render(request, 'app_mango/pest_disease_list.html', {
+        'pest_list': pest_list,
+        'disease_list': disease_list
+    })
 
+
+# Pest Detail View
+def pest_detail(request, pest_id):
+    pest = next((p for p in pest_list if p["id"] == pest_id), None)
+    if pest is None:
+        return HttpResponseNotFound("Page not found.")
+    return render(request, 'app_mango/pest_detail.html', {'pest': pest})
+
+
+# Disease Detail View
+def disease_detail(request, disease_id):
+    disease = next((d for d in disease_list if d["id"] == disease_id), None)
+    if disease is None:
+        return HttpResponseNotFound("Disease not found.")
+    return render(request, 'app_mango/disease_detail.html', {'disease': disease})
+
+
+# About Page View
 def about(request):
     return render(request, 'app_mango/about.html')
-
-def references(request):
-    return render(request, 'app_mango/references.html')
-
-
-
-
