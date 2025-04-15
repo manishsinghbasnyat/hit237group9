ece205c2f90ae201278c954992ab9984b3ec2cb8
Subodh <s372144@studentrs.cdu.edu.au>
2025-04-15 16:38:53 +0930

    defined a class in models.py for pest details

--- proj_mango/app_mango/models.py
+++ proj_mango/app_mango/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class PestInfo:
+    def __init__(self, id, name, short_description, long_description, image_url):
+        self.id = id
+        self.name = name
+        self.short_description = short_description
+        self.long_description = long_description
+        self.image_url = image_url
