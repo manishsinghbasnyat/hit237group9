1610fd8f667acb1524c3566e8a6988d92ba29446
Subodh <s372144@studentrs.cdu.edu.au>
2025-04-15 17:02:39 +0930

    added views for the website

--- proj_mango/app_mango/views.py
+++ proj_mango/app_mango/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
-from data import items
+from models import PestInfo, DiseaseInfo
+from data import pests, diseases
 
 # Create your views here.
 
-def pest_list(request):
-    # passes the list of pests & diseases to the template
-    return render(request, 'pests/pest_list.html', {'items': items})
+# from django.shortcuts import render
+from .data import pest_list, disease_list
+
+def home(request):
+    return render(request, 'app_mango/home.html')
+
+def about(request):
+    return render(request, 'app_mango/about.html')
+
+def references(request):
+    return render(request, 'app_mango/references.html')
+
+def pest_list_view(request):
+    context = {
+        'pests': pest_list,
+        'diseases': disease_list
+    }
+    return render(request, 'app_mango/pest_list.html', context)
+
+def pest_detail_view(request, item_id):
+    item = next((p for p in pest_list if p.id == item_id), None)
+    if item is None:
+        item = next((d for d in disease_list if d.id == item_id), None)
+    if item is None:
+        return render(request, 'app_mango/404.html')  # Or use Django's default 404
+    return render(request, 'app_mango/pest_detail.html', {'item': item})
