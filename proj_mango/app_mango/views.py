6d84e0ecb8158bc165ea86e1b9cecc9f8014e400
Subodh <s372144@studentrs.cdu.edu.au>
2025-04-15 17:05:41 +0930

    reordered functions

--- proj_mango/app_mango/views.py
+++ proj_mango/app_mango/views.py
@@ -10,18 +10,13 @@ from .data import pest_list, disease_list
 def home(request):
     return render(request, 'app_mango/home.html')
 
-def about(request):
-    return render(request, 'app_mango/about.html')
-
-def references(request):
-    return render(request, 'app_mango/references.html')
-
 def pest_list_view(request):
     context = {
         'pests': pest_list,
         'diseases': disease_list
     }
     return render(request, 'app_mango/pest_list.html', context)
+
 
 def pest_detail_view(request, item_id):
     item = next((p for p in pest_list if p.id == item_id), None)
@@ -30,3 +25,13 @@ def pest_detail_view(request, item_id):
     if item is None:
         return render(request, 'app_mango/404.html')  # Or use Django's default 404
     return render(request, 'app_mango/pest_detail.html', {'item': item})
+
+def about(request):
+    return render(request, 'app_mango/about.html')
+
+def references(request):
+    return render(request, 'app_mango/references.html')
+
+
+
+
