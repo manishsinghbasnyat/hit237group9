6e8c6675c35aad17dbad021e603af60379028105
Subodh <s372144@studentrs.cdu.edu.au>
2025-05-27 20:12:10 +0930

    registration view & profile

--- proj_mango/accounts/views.py
+++ proj_mango/accounts/views.py
@@ -1,20 +1,30 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from survey.models import SurveillanceRecord
+from accounts.forms import CustomUserCreationForm
 
-# Create your views here.
-
+# Registration view for new users
 def register_view(request):
     if request.user.is_authenticated:
         messages.info(request, "You are already registered and logged in.")
-        return redirect('home')  # or wherever you want to redirect logged-in users
+        return redirect('home') 
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)  # <-- use custom form
         if form.is_valid():
             form.save()
             messages.success(request, "Registration successful! You can now log in.")
             return redirect('login_view')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()  # custom form
+
     return render(request, 'accounts/register.html', {'form': form})
+
+@login_required
+def profile_view(request):
+    return render(request, 'accounts/profile.html')
+
+
+
