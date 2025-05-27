a9da405a08c099ec422ee611e1c9a051be42a71c
Subodh <s372144@studentrs.cdu.edu.au>
2025-05-27 20:15:46 +0930

    updated urls for dashboard

--- proj_mango/catalog/urls.py
+++ proj_mango/catalog/urls.py
@@ -7,8 +7,4 @@ urlpatterns = [
     path('<str:item_type>/<int:item_id>/', views.pest_detail_view, name='pest_detail'),
     path('about/', views.about, name='about'),
     path('references/', views.references, name='references'),
-    
-
 ]
-
-
