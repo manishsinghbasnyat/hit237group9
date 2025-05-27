b7a5c95cd3e31dec98a8f25c54db5513a3b45481
Subodh <s372144@studentrs.cdu.edu.au>
2025-05-27 20:10:53 +0930

    custom form to include additional attributes for registration

---
+++ proj_mango/accounts/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, label='First name')
+    last_name = forms.CharField(max_length=30, required=True, label='Last name')
+    email = forms.EmailField(max_length=254, required=True, label='Email address')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
