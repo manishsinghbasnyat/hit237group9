cf85a699b569f72e122b6f828043bd0e5e85690e
Subodh <s372144@studentrs.cdu.edu.au>
2025-05-27 13:55:51 +0930

    Added & implemented views for catalog

--- proj_mango/catalog/views.py
+++ proj_mango/catalog/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Pest, Disease
+from django.http import HttpResponseNotFound
 
-# Create your views here.
+# Home page view
+def home(request):
+    return render(request, 'catalog/home.html')
+
+# Pest/Disease list view
+def pest_list_view(request):
+    pests = Pest.objects.all()
+    diseases = Disease.objects.all()
+    context = {
+        'pests': pests,
+        'diseases': diseases
+    }
+    return render(request, 'catalog/pest_list.html', context)
+
+# Pest/Disease detail view with type (pest or disease)
+def pest_detail_view(request, item_type, item_id):
+    if item_type == 'pest':
+        item = get_object_or_404(Pest, pk=item_id)
+    elif item_type == 'disease':
+        item = get_object_or_404(Disease, pk=item_id)
+    else:
+        return HttpResponseNotFound("Page not Found")
+
+    return render(request, 'catalog/detail.html', {'item': item})
+
+# About page view
+def about(request):
+    return render(request, 'catalog/about.html')
+
+# References page view
+def references(request):
+    return render(request, 'catalog/references.html')
