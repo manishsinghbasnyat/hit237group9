c05886622f576dccc16c10953fdb8656ac753031
Subodh <s372144@studentrs.cdu.edu.au>
2025-05-27 19:58:42 +0930

    Modified paths

--- proj_mango/proj_mango/urls.py
+++ proj_mango/proj_mango/urls.py
@@ -7,10 +7,15 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # For catalog
     path('', include('catalog.urls')),  # All catalog routes (home, pests, about, etc.)
     
      # For survey
      path('survey/', include('survey.urls')),
+     
+     # For accounts
+     path('', include('accounts.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
