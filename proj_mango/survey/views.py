d81473f4c1d0d4eba85792f3a70532792fe0ee8a
Subodh <s372144@studentrs.cdu.edu.au>
2025-05-27 17:49:48 +0930

    updated url paths for survey

--- proj_mango/survey/views.py
+++ proj_mango/survey/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import SurveillanceRecordForm
 
-# Create your views here.
+def create_surveillancerecord_view(request):
+    if request.method == 'POST':
+        form = SurveillanceRecordForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record submitted successfully!")
+            return redirect('create_surveillancerecord_view')  # Redirect to clear POST data
+    else:
+        form = SurveillanceRecordForm()
+    return render(request, 'survey/surveillancerecord_form.html', {'form': form})
