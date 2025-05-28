bde3bb1f4d49650492f2c1c473d9cb5742e4d2d5
Subodh <s372144@studentrs.cdu.edu.au>
2025-05-29 08:32:48 +0930

    logic for user only filter

--- proj_mango/survey/forms.py
+++ proj_mango/survey/forms.py
@@ -4,7 +4,7 @@ from .models import SurveillanceRecord
 class SurveillanceRecordForm(forms.ModelForm):
     class Meta:
         model = SurveillanceRecord
-        fields = ['farm', 'date', 'inspector', 'pests_found', 'diseases_found', 'notes', 'image']
+        fields = ['farm', 'date', 'pests_found', 'diseases_found', 'notes', 'image']
         widgets = {
             'pests_found': forms.CheckboxSelectMultiple,
             'diseases_found': forms.CheckboxSelectMultiple,
