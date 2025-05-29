1095c2ba5237f7362ddfcba3973656f7a9e5a176
Subodh <s372144@studentrs.cdu.edu.au>
2025-05-29 09:56:17 +0930

    updated urls based on new views

--- proj_mango/survey/urls.py
+++ proj_mango/survey/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.survey_home_view, name='survey_home'), 
     path('new/', views.create_surveillancerecord_view, name='create_surveillancerecord_view'),
+    path('my/', views.my_surveys_view, name='my_surveys_view'),
+    path('detail/<int:pk>/', views.survey_detail_view, name='survey_detail_view'),
 ]
