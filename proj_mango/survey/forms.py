e86a4baf0aaffa8b7225c59d95fb0f93383c12f1
Subodh <s372144@studentrs.cdu.edu.au>
2025-05-29 09:54:45 +0930

    updated forms based on new model implementation

--- proj_mango/survey/forms.py
+++ proj_mango/survey/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import SurveillanceRecord
+from .models import SurveillanceRecord, Farm
+import datetime
+from django.utils import timezone
 
 class SurveillanceRecordForm(forms.ModelForm):
     class Meta:
@@ -8,4 +10,28 @@ class SurveillanceRecordForm(forms.ModelForm):
         widgets = {
             'pests_found': forms.CheckboxSelectMultiple,
             'diseases_found': forms.CheckboxSelectMultiple,
+            'date': forms.DateInput(
+                attrs={
+                    'type': 'date',
+                    'max': timezone.localdate().isoformat(),
+                    'min': '2000-01-01'
+                }
+            ),
         }
+        help_texts = {
+            'date': 'Select a date between 01/01/2000 and today.',
+        }
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)  # Accept 'user' kwarg!
+        super().__init__(*args, **kwargs)
+        if user is not None:
+            self.fields['farm'].queryset = Farm.objects.filter(owner=user)
+
+    def clean_date(self):
+        date = self.cleaned_data['date']
+        min_date = datetime.date(2000, 1, 1)
+        max_date = timezone.localdate()
+        if date < min_date or date > max_date:
+            raise forms.ValidationError(f"Date must be between {min_date.strftime('%d/%m/%Y')} and today.")
+        return date
